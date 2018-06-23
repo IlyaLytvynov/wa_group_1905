93984644341e7da02213da8ede2b70130d9c4c34
ilya.lytvynov <ilya.lytvynov@egt-ua.com>
2018-06-23 12:17:51 +0300

    Publish

--- config/prod.js
+++ config/prod.js
@@ -3,15 +3,13 @@ const webpack = require('webpack');
 
 const prod = (config, _path) => {
     console.info("ENV: PROD");
-    // To enable uglify and decreasing bundle weight please uncommnet next lines
-
-    // const uglify = [new webpack.optimize.UglifyJsPlugin({
-    //     compress: {
-    //         warnings: false,
-    //         drop_console: false,
-    //     }
-    // })];
-    // config.plugins = config.plugins.concat(uglify);
+    const uglify = [new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            drop_console: false,
+        }
+    })];
+    config.plugins = config.plugins.concat(uglify);
     config.output.path =  _path;
 
     return config;
