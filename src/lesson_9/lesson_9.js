6ef0221042107803b1787ef0b8dde7b9c1d13417
ilya.lytvynov <ilya.lytvynov@egt-ua.com>
2018-06-24 14:50:57 +0300

    Lesson 10

--- src/lesson_9/lesson_9.js
+++ src/lesson_9/lesson_9.js
@@ -1,6 +1,15 @@
 import './lesson_9.scss';
+import { lighter } from './scripts/lighter';
 
-function notificationBar () {
+const lighterElement= document.querySelector('#lighter1');
+const lighterElement2 = document.querySelector('#lighter2');
+const lighterElement3 = document.querySelector('#lighter3');
+
+lighter(lighterElement);
+lighter(lighterElement2);
+lighter(lighterElement3);
+
+function notificationBar() {
 
   const notificationBarElement = document.querySelector('.notification-bar');
   const control = document.querySelector('.notification-bar__btn');
